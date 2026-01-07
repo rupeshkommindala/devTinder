8c1768735c3744499b5524c10d1f53864cbc4860
rupeshkommindala <rupeshkommindala@gmail.com>
2026-01-07 15:00:50 +0530

    multiple-route-handlers

--- homework.md
+++ homework.md
@@ -18,7 +18,12 @@
 - Order of the routes matter a lot
 - Install Postman app and make a workspace/collection > test API call
 - Write logic to handle GET, POST, PATCH, DELETE API Calls and test them on Postman
-- Explore routing and use of ?, +, (), * in the routes
+- Explore routing and use of ?, +, (), * , in the routes
 - Use of regex in routes /a/ , /.*fly$/
 - Reading the query params in the routes
 - Reading the dynamic routes
+
+- Multiple Route Handlers - Play with the code
+- next()
+- next function and errors along with res.send()
+- app.use("/route",rH,rH2,rH3,rH4,rH5)
--- src/app.js
+++ src/app.js
@@ -1,10 +1,33 @@
 const express = require("express");
 const app = express();
 
-app.get("/user/:userId", (req, res) => {
-  console.log(req.params);
-  res.send({ firstName: "Rupesh", lastName: "Kommindala" });
-});
+app.use("/user", [
+  (req, res, next) => {
+    console.log("Route Handler 1");
+    //res.send("1st Response");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Route Handler 2");
+    //res.send("2nd Response");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Route Handler 3");
+    //res.send("3rd Response");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Route Handler 4");
+    //res.send("4th Response");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Route Handler 5");
+    //res.send("5th Response");
+    next();
+  },
+]);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
