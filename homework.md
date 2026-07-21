e1bd7ad961bc3c49b3ae9e12f070c6604aee4a1d
rupeshkommindala <rupeshkommindala@gmail.com>
2026-07-21 09:47:59 +0530

    created a express server

--- homework.md
+++ homework.md
@@ -3,7 +3,7 @@
 - node_modules, package.json, package-lock.json
 - Install express
 - Create a server
-- Listen to the port 7777
+- Listen to the port 3000
 - Write request handlers for /test, /hello
 - Install nodemon and update scripts inside package.json
 - What are dependencies
@@ -18,8 +18,8 @@
 - Order of the routes matter a lot
 - Install Postman app and make a workspace/collection > test API call
 - Write logic to handle GET, POST, PATCH, DELETE API Calls and test them on Postman
-- Explore routing and use of ?, +, (), * , in the routes
-- Use of regex in routes /a/ , /.*fly$/
+- Explore routing and use of ?, +, (), \* , in the routes
+- Use of regex in routes /a/ , /.\*fly$/
 - Reading the query params in the routes
 - Reading the dynamic routes
 
--- src/app.js
+++ src/app.js
@@ -1,34 +1,18 @@
 const express = require("express");
 const app = express();
 
-app.use("/user", [
-  (req, res, next) => {
-    console.log("Route Handler 1");
-    //res.send("1st Response");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Route Handler 2");
-    //res.send("2nd Response");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Route Handler 3");
-    //res.send("3rd Response");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Route Handler 4");
-    //res.send("4th Response");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Route Handler 5");
-    //res.send("5th Response");
-    next();
-  },
-]);
+app.use("/", (req, res) => {
+  res.send("Hello from the Dashboard!");
+});
+
+app.use("/hello", (req, res) => {
+  res.send("Hello Hello Hello!");
+});
+
+app.use("/test", (req, res) => {
+  res.send("Hello from the Test!");
+});
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log("Server is successfully running on port 3000");
 });
