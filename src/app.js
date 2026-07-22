3666dc5c0382426478b23daba32aa3ad8aabee67
rupeshkommindala <rupeshkommindala@gmail.com>
2026-07-22 15:50:38 +0530

    exploring get, post, delete api calls

--- src/app.js
+++ src/app.js
@@ -2,6 +2,18 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Rupesh", lastName: "K" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("Data successfully saved to DB");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Deleted successfully");
+});
+
 app.get("/user/:userId/:name/:password", (req, res) => {
   console.log(req.params);
   res.send({ firstName: "Rupesh", lastName: "Kommindala" });
