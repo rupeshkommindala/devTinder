a94d3f50c6b9a7720f76d5dfc305d1e64d57018e
rupeshkommindala <rupeshkommindala@gmail.com>
2026-07-24 20:54:42 +0530

    exploring get call

--- src/app.js
+++ src/app.js
@@ -6,19 +6,6 @@ app.get("/user", (req, res) => {
   res.send({ firstName: "Rupesh", lastName: "K" });
 });
 
-app.post("/user", (req, res) => {
-  res.send("Data successfully saved to DB");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("Deleted successfully");
-});
-
-app.get("/user/:userId/:name", (req, res) => {
-  console.log(req.params);
-  res.send({ firstName: "Rupesh", lastName: "Kommindala" });
-});
-
 app.listen(3000, () => {
   console.log("Server is successfully running on port 3000");
 });
